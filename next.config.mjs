0a1bb3220c3275a9dfddb5ca42ad64b8bf465fab
Alexandre Camillo <alexandre.camillo@ic.ufrj.br>
2026-05-07 22:35:42 -0300

    fix(deps): move build-time deps to devDependencies

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
-  experimental: { serverActions: { allowedOrigins: ['localhost:3000'] } },
   poweredByHeader: false,
 };
 
